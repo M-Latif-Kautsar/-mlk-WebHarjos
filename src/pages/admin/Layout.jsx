caa3e587680391644abf70819c30e77ce59a622d
Latif <m.latifkautsar@gmail.com>
2023-07-25 21:55:35 +0700

    edit layout

--- src/pages/admin/Layout.jsx
+++ src/pages/admin/Layout.jsx
@@ -57,7 +57,7 @@ export default function Layout() {
     <div className="flex h-screen bg-gray-50 dark:bg-gray-900 false">
       <aside className="z-30 flex-shrink-0 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 lg:block">
         <a
-          href="/#"
+          href="/admin"
           className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200"
         >
           The Harjos Java Resto
